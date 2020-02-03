3a1c0bc892a05fd409a752f270277cdbfe68b0e1
Matteo Bortolotto <yamatteo@gmail.com>
2020-02-03 12:16:15 -0300

    Set mountain ip

--- flask_app.py
+++ flask_app.py
@@ -1,11 +1,17 @@
-
-# A very simple Flask Hello World app for you to get started with...
-
 from flask import Flask
+from flask import render_template
+from flask import request
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def hello_world():
-    return 'Hello from Flask!'
+    return render_template("landing.html", mountain_ip=getattr(app, "mountain_ip", None))
 
+
+@app.route("/set_mountain", methods=["POST"])
+def set_mountain():
+    from json import loads
+    setattr(app, "mountain_ip", loads(request.data)["mountain_ip"])
+    return "Ok"
